a854fb017062d8ccc0233eb3b5c164ad1ed98b63
Omar <Omar.abdi.2022@student.lhschools.org>
2019-02-08 08:01:03 -0500

    Create Intelij

---
+++ src/Magic8Ball.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+    public static void main(String[] args) {
+
+    }
+
+   //Declare
+    String NoDoubt,forSure,ofCourse,yes,possibly;
+    String Reconsider,iWouldnt,Dont,no,maybe;
+    int number1,number2;
+   if(N)
+
+  //initilizing
+
+
+
+}
